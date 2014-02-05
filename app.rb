4c1c1e1b8e050cc673bc04a2df713112ac5bca8e
Peter Saxton <peterhsaxton@gmail.com>
2014-02-05 16:12:48 +0000

    UserController class integrated

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
+require './lib/usersController'
 
 class HomeController < Sinatra::Base
 	get '/' do
 		"Work in progress Dave"
+	end
+
+	get '/users' do
+		UsersController.call(env)
 	end
 end
---
+++ lib/usersController.rb
@@ -0,0 +1,5 @@
+class UsersController < Sinatra::Base
+	get '/users' do
+		"Work in progress mike"
+	end
+end
